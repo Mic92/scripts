b663c6f133c1a5b79621ace3d5a87173f88bbfdf
Joerg T. (Mic92) <jthalheim@gmail.com>
2010-05-04 12:30:47 +0200

    comment out the wallpaper_cmd instead of delete it

--- patchawesome.sh
+++ patchawesome.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # remove wallpaper setter in standart theme to use awsetbg with my own pics
-sed -i -e 's/theme.wallpaper_cmd = { "awsetbg \/usr\/share\/awesome\/themes\/default\/background.png\" }//g' /usr/share/awesome/themes/default/theme.lua
+sed -i -e 's/\(theme.wallpaper_cmd = {.*}\)/--\1/g' /usr/share/awesome/themes/default/theme.lua
 # bytecompile all librarys
 find  /usr/share/awesome/lib -name \*.lua -execdir luac -o {} {} \;
