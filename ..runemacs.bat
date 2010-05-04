d40c92ba9605daff3b7380a7a8233dc87d8bf3ba
Joerg T. (Mic92) <jthalheim@gmail.com>
2010-05-04 12:32:01 +0200

    Intial commit

---
+++ ..runemacs.bat
@@ -0,0 +1,12 @@
+@echo off
+rem Wrapper to start emacs from a USB-stick on win32-machines
+set HOME=%CD%\config
+rem Add build tools
+set PATH=%PATH%;%CD%\..\MinGW\bin
+set PATH=%PATH%;%CD%\..\FPC\2.2.4\bin\i386-win32
+rem Local unix tools
+set PATH=%PATH%;C:\Cygwin\bin
+set MANPATH=C:\Cygwin\usr\man;C:\Cygwin\usr\local\man
+set SHELL=C:/Cygwin/bin/bash.exe
+rem Perform the start
+%CD%\bin\runemacs
