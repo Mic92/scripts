2a6d1e701fd18ef7d0aca09e08e9cae368d6c7d2
Jörg Thalheim <joerg@higgsboson.tk>
2014-02-15 21:13:42 +0100

    delete legacyscripts

--- old/checkgmail.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-unread=`curl -s -u "yourname@gmail.com:secret-password"  -X GET https://mail.google.com/mail/feed/atom | sed -ne 's/.*<fullcount>\(.*\)<\/fullcount>.*/\1/p' 2> /dev/null`
-echo You have $unread mails.
